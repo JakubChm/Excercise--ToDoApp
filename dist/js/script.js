162688b2b3ecc26ca235b62805ae9faa58fc271a
Jakub Chmura <jakub.chmura@wp.pl>
2019-04-20 10:40:19 +0200

    some js code

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,5 @@
-const lol = lol;
+const searchInput = document.querySelector('#search-field');
+const addInput = document.querySelector('#add-field');
+const addButton = document.querySelector('.add-button')
+const searchButton = document.querySelector('.search-button')
+const ulHtml = document.querySelector('.todo-ul');
