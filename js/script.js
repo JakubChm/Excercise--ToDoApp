3e81f0f38a08526639f07aebd34f54e67834467b
Jakub Chmura <jakub.chmura@wp.pl>
2019-04-24 14:13:09 +0200

    add and remove

--- js/script.js
+++ js/script.js
@@ -2,52 +2,71 @@ const addInput = document.getElementById('add-field');
 const searchInput = document.getElementById('search-field');
 const addBtn = document.querySelector('.add-button');
 const searchBtn = document.querySelector('.search-button');
-const listContainer = document.querySelector('.todo-list__tasks');
+const ul = document.querySelector('.todo-list__tasks');
 const numberOfTasks = document.querySelector('.tasks-number');
 const completedTasts = document.querySelector('.doneTask-Number');
-const notCompleteTasts = document.querySelector('.toCompleteTask-Number');
+const notCompletedTasts = document.querySelector('.toCompleteTask-Number');
 
-let liItems
-let buttonList
-
-const setTaskNumber = () => {
-  numberOfTasks.textContent = `${liItems.length}`;
-  notCompleteTasts.textContent = `${liItems.length}`;
+const todoList = [];
+// Add
+const createTodoElement = () => {
+  const liElement = document.createElement('li');
+  const deleteBtn = document.createElement('button');
+  liElement.className = "task";
+  liElement.textContent = `${addInput.value}`;
+  ul.appendChild(liElement)
+  deleteBtn.className = "delete-btn"
+  deleteBtn.textContent = "Delete";
+  liElement.appendChild(deleteBtn)
+  todoList.push(liElement)
 }
+const addError = () => {
+  if (addInput.value === "") return alert('You can\'t add an empty string to your TODO list. Please try one more time.');
+}
+const updateList = () => {
+  ul.textContent = "";
+  todoList.forEach((todoElement, id) => {
+    ul.appendChild(todoElement)
+    todoElement.dataset.id = id;
+  });
+  setTaskNumber();
+}
+const setTaskNumber = () => {
+  numberOfTasks.textContent = todoList.length;
+}
+//Remove
+const removeTask = (e) => {
+  e.target.parentNode.remove();
+  const index = e.target.parentNode.dataset.id;
+  todoList.splice(index, 1);
+  updateList();
+}
+// Search
+const searchError = () => {
+  if (searchInput.value === "") return alert('You can\'t search nothing lol. Please try one more time.');
+  if (todoList.length === 0) return alert('You can\'t search on empty list. Please try one more time.');
+  console.log('weszło');
+}
+
+
+
+
+
+
 
 const addTask = (e) => {
   e.preventDefault();
-  if (addInput.value === "") {
-    return alert('You can\'t add an empty string to your TODO list. Please try with one more time.');
-  } else {
-    //
-    // Create li element from input value
-    //
-    const liElement = document.createElement('li');
-    listContainer.appendChild(liElement).innerHTML = `${addInput.value} <button type="submit">Delete</button>`;
-    //
-    // Clear add input
-    //
-    addInput.value = "";
-    //
-    // Update array with button and li elements
-    //
-    liItems = [...document.querySelectorAll('.todo-list__tasks li')]
-    buttonList = [...document.querySelectorAll('.todo-list__tasks li button')];
-    //
-    // Create dataset and classes to list and button elements
-    //
-    for (let i = 0; i < liItems.length; i++) {
-      liItems[i].dataset.id = `${i}`;
-      buttonList[i].dataset.id = `${i}`;
-      buttonList[i].classList.add('delete-btn')
-    };
-  }
-  setTaskNumber();
+  addError();
+  createTodoElement();
+  updateList();
+  addInput.value = "";
+  document.querySelectorAll('.delete-btn').forEach(btnElement => btnElement.addEventListener('click', removeTask));
 }
 
+const searchTask = (e) => {
+  e.preventDefault();
+  searchError();
 
-
-
-
+}
 addBtn.addEventListener('click', addTask)
+searchBtn.addEventListener('click', searchTask)
